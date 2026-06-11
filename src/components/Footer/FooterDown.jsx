5f2dbea05da1a4a9fbe975d34d83970c72d774c2
RezaFrontEndDeveloper <rezablackcamel@gmail.com>
2026-06-12 01:13:57 +0330

    fixed footer in mobile

--- src/components/Footer/FooterDown.jsx
+++ src/components/Footer/FooterDown.jsx
@@ -7,10 +7,10 @@ import { MdOutlineEmail } from "react-icons/md";
 
 function FooterDown() {
   return (
-    <div className="w-full flex bg-red-700 p-8  border-b-2 border-white">
+    <div className="w-full flex-col sm:flex-row bg-red-700 p-8  border-b-2 border-white">
       <div className="flex w-full flex-1 flex-col gap-10 justify-start items-center">
         <IconTextFlex icon={<MdLocationOn size={30} color="white" />}>Mazandaran Province, Shahrsari, Industrial Park No. 1, Shamsashmal Factory (Happiness)</IconTextFlex>
-        <div className="flex justify-start items-center gap-4 w-full">
+        <div className="flex justify-center sm:justify-start items-center gap-4 w-full">
           <CiInstagram size={30} color="white" />
           <FaTelegram size={30} color="white" />
         </div>
