10a4cb9ca609f42bf971ad90e1e1f86a2dce25d6
RezaFrontEndDeveloper <rezablackcamel@gmail.com>
2026-06-12 20:46:18 +0330

    fixed hiro section 1

--- src/components/mainPage/SwiperImageMain.jsx
+++ src/components/mainPage/SwiperImageMain.jsx
@@ -4,6 +4,8 @@ import "swiper/css/pagination";
 import imageOne from "../../assets/hiro1.webp";
 import imageTwo from "../../assets/hiro2.webp";
 import imageThree from "../../assets/hiro3.webp";
+import imageMobile from "../../assets/hiromobile.webp";
+import imageMobile2 from "../../assets/hiromobile2.webp";
 import "../../index.css";
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -20,13 +22,16 @@ function SwiperImageMain() {
         }}
         loop={true}>
         <SwiperSlide>
-          <img src={imageOne} alt="" />
+          <img src={imageMobile} className=" w-full sm:hidden" alt="" />
+          <img src={imageOne} alt="" className="hidden sm:block" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={imageTwo} alt="" />
+          <img src={imageMobile2} className="w-full sm:hidden" alt="" />
+          <img src={imageTwo} alt="" className="hidden sm:block" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={imageThree} alt="" />
+          <img src={imageMobile} className="w-full sm:hidden" alt="" />
+          <img src={imageThree} alt="" className="hidden sm:block" />
         </SwiperSlide>
       </Swiper>
     </div>
--- src/components/mainPage/TitleMain.jsx
+++ src/components/mainPage/TitleMain.jsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 function TitleMain({ children }) {
   return (
     <div>
