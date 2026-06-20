9427ee2baf2ef29c5c05e8ed31e1af71aaad07bf
RezaFrontEndDeveloper <rezablackcamel@gmail.com>
2026-06-20 12:21:12 +0330

    fixed saving data in localStorage

--- src/store/useBasketStore.jsx
+++ src/store/useBasketStore.jsx
@@ -1,18 +1,28 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useBasketStore = create((set) => ({
-  basket: [],
+const useBasketStore = create(
+  persist(
+    (set) => ({
+      basket: [],
 
-  addToBasket: (item) =>
-    set((state) => ({
-      basket: [...state.basket, item],
-    })),
+      addToBasket: (item) =>
+        set((state) => ({
+          basket: [...state.basket, item],
+        })),
 
-  deleteItem: (id) => {
-    set((state) => ({
-      basket: state.basket.filter((item) => item.id !== id),
-    }));
-  },
-}));
+      deleteItem: (id) =>
+        set((state) => ({
+          basket: state.basket.filter((item) => item.id !== id),
+        })),
+    }),
+    {
+      name: "basket",
+      partialize: (state) => ({
+        basket: state.basket,
+      }),
+    }
+  )
+);
 
 export default useBasketStore;
